01d8f5d1e07cecca68169c6337b8db4120eb7975
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2024-04-23 19:53:07 +0300

    Add support for custom colors, remove folder name from file name result

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import string
 import threading as th
-import qrcode as qr
+import qrcode
 import time
 import datetime
 
@@ -14,7 +14,11 @@ def create_qr_code():
         os.makedirs("QR-codes")
 
     # Create QR code
-    img = qr.make(content_field.get())
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+    )
+    img = qr.make_image(fill_color=code_color.get(), back_color=background_color.get())
 
     # Get timestamp
     ts = time.time()
@@ -33,13 +37,13 @@ def create_qr_code():
     image_area.image = image
 
     # Change status text
-    status_text = "QR created with name: " + image_name_string
+    status_text = "QR created with name: " + "QR-" + date_string + ".png"
     content_label.configure(text=status_text)
 
 # Create window, set size and window title
 window = tk.Tk()
 window.title("Parrot QR Creator")
-window.geometry("400x600")
+window.geometry("450x700")
 window.iconbitmap("res/parrot-icon.ico")
 
 # Image file
@@ -47,14 +51,36 @@ image = Image.open("res/placeholder-image.jpg")
 image = ImageOps.fit(image, (300, 300))
 image = ImageTk.PhotoImage(image)
 
+# Empty space
+empty_label = tk.Label(window, text="")
+empty_label.pack(side="top", pady=1)
+
 # Input text
-content_label = tk.Label(window, text="Text to embed:", font=('Arial', 15), height = 1)
-content_label.pack(side="top", pady=20)
+content_label = tk.Label(window, text="Text to embed:", font=('Arial', 13), height = 1)
+content_label.pack(side="top", pady=5, padx=10)
 content_field = tk.Entry(window, width=50)
 content_field.pack(side="top",)
 
+# Code color option
+code_color_label = tk.Label(window, text="Code color:", font=('Arial', 13), height = 1)
+code_color_label.pack(side="top", pady=5)
+code_color_options = ["black", "white", "grey", "red", "crimson", "blue", "cyan", "green", "palegreen", "olive", "yellow", "gold", "orange", "chocolate", "brown", "pink", "violet", "purple"]
+code_color = tk.StringVar()
+code_color.set(code_color_options[0])
+code_color_dropdown = tk.OptionMenu(window, code_color, *code_color_options)
+code_color_dropdown.pack()
+
+# Background color option
+background_color_label = tk.Label(window, text="Background color:", font=('Arial', 13), height = 1)
+background_color_label.pack(side="top", pady=5)
+background_color_options = ["white", "black", "grey", "red", "crimson", "blue", "cyan", "green", "palegreen", "olive", "yellow", "gold", "orange", "chocolate", "brown", "pink", "violet", "purple"]
+background_color = tk.StringVar()
+background_color.set(background_color_options[0])
+background_color_dropdown = tk.OptionMenu(window, background_color, *background_color_options)
+background_color_dropdown.pack()
+
 # Start button
-start_button = tk.Button(window, text="Create QR code", font=('Arial', 15), command=lambda: th.Thread(target=create_qr_code).start(), height = 1, width = 15)
+start_button = tk.Button(window, text="Create QR code", font=('Arial', 14), command=lambda: th.Thread(target=create_qr_code).start(), height = 1, width = 15)
 start_button.pack(side="top", pady=20)
 
 # Latest QR code or placeholder image
