a7e274def9c04ad444d5393213435037d7cd034e
Lumisumu <64976539+Lumisumu@users.noreply.github.com>
2024-04-24 19:01:46 +0300

    Switch from dropdown to tkinter color picker, fix adding label text to qr generation, update readme

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import colorchooser
 from PIL import Image, ImageTk, ImageOps
 import os
 import subprocess
@@ -18,7 +19,10 @@ def create_qr_code():
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
     )
-    img = qr.make_image(fill_color=code_color.get(), back_color=background_color.get())
+    qr.add_data(content_field.get())
+    global code_color
+    global background_color
+    img = qr.make_image(fill_color=code_color, back_color=background_color)
 
     # Get timestamp
     ts = time.time()
@@ -39,6 +43,17 @@ def create_qr_code():
     # Change status text
     status_text = "QR created with name: " + "QR-" + date_string + ".png"
     content_label.configure(text=status_text)
+
+def choose_code_color():
+    global code_color
+    code_color = colorchooser.askcolor()[0]
+    print(code_color)
+
+def choose_background_color():
+    global background_color
+    background_color = colorchooser.askcolor()[0]
+    print(background_color)
+
 
 # Create window, set size and window title
 window = tk.Tk()
@@ -64,20 +79,14 @@ content_field.pack(side="top",)
 # Code color option
 code_color_label = tk.Label(window, text="Code color:", font=('Arial', 13), height = 1)
 code_color_label.pack(side="top", pady=5)
-code_color_options = ["black", "white", "grey", "red", "crimson", "blue", "cyan", "green", "palegreen", "olive", "yellow", "gold", "orange", "chocolate", "brown", "pink", "violet", "purple"]
-code_color = tk.StringVar()
-code_color.set(code_color_options[0])
-code_color_dropdown = tk.OptionMenu(window, code_color, *code_color_options)
-code_color_dropdown.pack()
+code_color = "black"
+code_color_button = tk.Button(window, text="Pick code color", command=choose_code_color).pack()
 
 # Background color option
 background_color_label = tk.Label(window, text="Background color:", font=('Arial', 13), height = 1)
 background_color_label.pack(side="top", pady=5)
-background_color_options = ["white", "black", "grey", "red", "crimson", "blue", "cyan", "green", "palegreen", "olive", "yellow", "gold", "orange", "chocolate", "brown", "pink", "violet", "purple"]
-background_color = tk.StringVar()
-background_color.set(background_color_options[0])
-background_color_dropdown = tk.OptionMenu(window, background_color, *background_color_options)
-background_color_dropdown.pack()
+background_color = "white"
+background_color_button = tk.Button(window, text="Pick background color", command=choose_background_color).pack()
 
 # Start button
 start_button = tk.Button(window, text="Create QR code", font=('Arial', 14), command=lambda: th.Thread(target=create_qr_code).start(), height = 1, width = 15)
